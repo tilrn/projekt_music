45dbe1db4e9536a1ae58f9fdf2a418679762351e
Tilen Hostnik <tilen.hostnik2@scv.si>
2021-01-04 15:02:08 +0100

    Add project files.

---
+++ projekt_music/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+
+namespace projekt_music
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lol();
+        }
+        connection baza = new connection();
+        string connect = connection.connect();
+        public void lol()
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection(connect))
+            {
+                con.Open();
+
+                NpgsqlCommand com = new NpgsqlCommand("SELECT * FROM kraji", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    string ime = reader.GetString(1);
+                    comboBox1.Items.Add(ime);
+                }
+                lol
+
+                con.Close();
+            }
+        }
+    }
+}
---
+++ projekt_music/connection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace projekt_music
+{
+    class connection
+    {
+        public static string connect()
+        {
+            var uriString = ConfigurationManager.AppSettings["ELEPHANTSQL_URL"] ?? "postgres://sornobua:c3AfGBt963qUP5wnI7Mej757wn9Nm0JO@kandula.db.elephantsql.com:5432/sornobua";
+            var uri = new Uri(uriString);
+            var db = uri.AbsolutePath.Trim('/');
+            var user = uri.UserInfo.Split(':')[0];
+            var passwd = uri.UserInfo.Split(':')[1];
+            var port = uri.Port > 0 ? uri.Port : 5432;
+            var connStr = string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4}",
+                uri.Host, db, user, passwd, port);
+
+            return connStr;
+
+
+        }
+    }
+}

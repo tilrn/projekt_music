72e75049f276f5c35aa1ab3497af7697fc53739b
Tilen Hostnik <tilen.hostnik2@scv.si>
2021-03-14 23:35:22 +0100

    delo na krajih

--- projekt_music/Kraji.cs
+++ projekt_music/Kraji.cs
@@ -41,7 +41,7 @@ namespace projekt_music
 
         private void button3_Click(object sender, EventArgs e)
         {
-            bazaa.DELETEKraja(te);
+            //bazaa.DELETEKraja();
         }
 
         private void button4_Click(object sender, EventArgs e)
